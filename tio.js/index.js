d708c9fdaa47e0da5dc1568ac08bef75115b9623
One Time Productions Time <jaysentsao@outlook.com>
2021-03-24 02:15:06 +0000

    Init

--- tio.js/index.js
+++ tio.js/index.js
@@ -6,9 +6,9 @@ encode(language, text, input) {
     return "Vlang\0001\000"+language+"\000F.input.tio\000"+input.length+"\000"+input+"\000"+"F.code.tio\000"+text.length+"\000"+text+"R";
 }
 
-module.exports = async tio(language, code, input) {
+module.exports = async function tio(language, code, input) {
     const encoded = encode(language, code, input); 
-    const res = compress(encoded, function(err, compressed) => {
+    const res = compress(encoded, function(err, compressed) {
         const response = await fetch(BASE, {
             method: "POST",
             body: compressed
