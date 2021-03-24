23ef6512080efaa52d66fa17b8338920f93ee806
One Time Productions Time <jaysentsao@outlook.com>
2021-03-24 02:22:50 +0000

    set input default value

--- tio.js/index.js
+++ tio.js/index.js
@@ -6,7 +6,7 @@ function encode(language, text, input) {
     return "Vlang\0001\000"+language+"\000F.input.tio\000"+input.length+"\000"+input+"\000"+"F.code.tio\000"+text.length+"\000"+text+"R";
 }
 
-module.exports = async function tio(language, code, input) {
+module.exports = async function tio(language, code, input="") {
     const encoded = encode(language, code, input); 
     const compressed = await inflate(encoded);
     const res = await fetch(BASE, {
