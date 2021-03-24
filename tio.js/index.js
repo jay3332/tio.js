26557679134848739ccf04c979d72f5b8af37c21
One Time Productions Time <jaysentsao@outlook.com>
2021-03-24 02:21:36 +0000

    Final

--- tio.js/index.js
+++ tio.js/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const { inflate } = require("zlib");
 const BASE = 'https://tio.run/cgi-bin/run/api/';
 
-encode(language, text, input) {
+function encode(language, text, input) {
     return "Vlang\0001\000"+language+"\000F.input.tio\000"+input.length+"\000"+input+"\000"+"F.code.tio\000"+text.length+"\000"+text+"R";
 }
 
